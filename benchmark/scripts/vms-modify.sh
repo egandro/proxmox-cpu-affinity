f14d04a5ad18156d48956e7291fccb2ce7b60e99
Harald Fielker <harald.fielker@gmail.com>
2025-12-28 00:35:23 +0100

    modify vms hardware

---
+++ benchmark/scripts/vms-modify.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e
+
+SCRIPTDIR="$(dirname "$0")"
+
+. ${SCRIPTDIR}/../config.sh
+
+STORAGE="${STORAGE:-local-zfs}"
+
+if [ ! -f "$1" ]; then
+    echo "Error: User script required."
+    exit 1
+fi
+
+USER_SCRIPT="$1"
+
+NUM_VMS="${BENCHMARK_NUM_VMS:-2}"
+START_VMID="${BENCHMARK_START_VMID:-200}"
+
+echo "Modifying $NUM_VMS dummy VMs starting from ID $START_VMID..."
+
+for i in $(seq 1 $NUM_VMS); do
+    # Calculate the VMID
+    VMID=$((START_VMID + i - 1))
+
+    echo "[$i/$NUM_VMS] Processing VM $VMID..."
+
+    if ! qm status $VMID | grep -q "status: stopped"; then
+        echo "Error: VM $VMID is not stopped."
+        exit 1
+    fi
+
+    echo "Running user script $USER_SCRIPT on VM $VMID with storage $STORAGE..."
+    $USER_SCRIPT "$VMID" "$STORAGE"
+done
